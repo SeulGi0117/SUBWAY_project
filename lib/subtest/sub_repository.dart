2af050d493b87e5a220ce46a1ed0f9bb9da75b06
SeulGi0117 <jusanha17@gmail.com>
2022-12-18 23:00:55 +0900

    Locations: No module for http - 에러

--- lib/subtest/sub_repository.dart
+++ lib/subtest/sub_repository.dart
@@ -30,9 +30,9 @@ class SubRepoitory {
     final body = convert.utf8.decode(response.bodyBytes);
 
     // var content = jsonDecode(utf8.decode(response.bodyBytes));
-    // print("1__________________________________1");
-    // print(content['data']['route']);
-    // print("2__________________________________2");
+    print("1__________________________________1");
+    print(body);
+    print("2__________________________________2");
 
     // 필요한 데이터 찾기
 
@@ -51,36 +51,13 @@ class SubRepoitory {
     // final jsonSub = json['body'];
     // Sub sub = Sub.fromJson(json);
 
-//     const jsonString =
-//         '{"text": "foo", "value": 1, "status": false, "extra": null}';
-
-//     final data = jsonDecode(jsonString);
-//     print(data['text']); // foo
-//     print(data['value']); // 1
-//     print(data['status']); // false
-//     print(data['extra']); // null
-
-//     const jsonArray = '''
-//   [{"text": "foo", "value": 1, "status": true},
-//    {"text": "bar", "value": 2, "status": false}]
-// ''';
-
-//     final List<dynamic> dataList = jsonDecode(jsonArray);
-//     print(dataList[0]); // {text: foo, value: 1, status: true}
-//     print(dataList[1]); // {text: bar, value: 2, status: false}
-
-//     final item = dataList[0];
-//     print(item['text']); // foo
-//     print(item['value']); // 1
-//     print(item['status']); // false
-
     // 필요한 데이터 그룹이 있다면
     if (jsonSub['start_cd'] != null) {
       // map을 통해 데이터를 전달하기 위해 객체인 List로 만든다.
       List<dynamic> list = jsonSub['start_cd'];
 
       // map을 통해 Sub형태로 item을  => Sub.fromJson으로 전달
-      return list.map<Sub>((start_cd) => Sub.fromJson(start_cd)).toList();
+      return list.map<Sub>((item) => Sub.fromJson(item)).toList();
 
       }
 
